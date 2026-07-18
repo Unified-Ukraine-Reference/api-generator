10eb3dd06226d12842c028532d7e51b226bb8744
uncle-Yevhenii <yevhenii.katsimon@gmail.com>
2026-07-18 13:42:08 +0300

    chore: edit config eslint and ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,18 +4,22 @@ import tseslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettier,
   {
     files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'],
     languageOptions: { globals: globals.node },
   },
-  pluginJs.configs.recommended,
-  eslintPluginPrettier,
   {
     files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'],
     rules: {
       'capitalized-comments': ['error', 'always'],
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: { 'no-undef': 'off' },
   },
 ];
